b3d1f54c247a6b8a96f66a14d365a94be007cf77
Melanie Nolker <melanie.nolker@kiewit.com>
2021-02-21 15:40:51 -0600

    bubble chart complete

--- static/js/app.js
+++ static/js/app.js
@@ -30,8 +30,10 @@ for(var i = 0; i < options.length; i++) {
 // init();
 
 function optionChanged() {
+    // create variable for drop down selection of ID
     var dropdownSelection = d3.select("#selDataset").node().value; 
 
+    //
     d3.json("samples.json").then((importedData)=> {
         function filterId(individual) {
             return individual.id === dropdownSelection;
@@ -42,6 +44,8 @@ function optionChanged() {
         var filteredId = samplesData.filter(filterId);
             console.log(filteredId);
         
+    //__________________________________________________________________
+        // bar plot data transformation
             var sampleValuesFiltered = filteredId[0].sample_values.slice(0,10).reverse();
         
             var sampleIdsFiltered = filteredId[0].otu_ids.slice(0,10).reverse();
@@ -50,106 +54,61 @@ function optionChanged() {
             console.log(sampleIds);
             console.log(sampleValuesFiltered);
 
-        // Create your trace.
-        var trace = {
+        // Create trace for bar chart
+        var traceBar = {
             x: sampleValuesFiltered,
             y: sampleIds,
             type: 'bar',
             orientation: 'h'
         };
 
-        // Create the data array for our plot
-        var data = [trace];
+        // Create the data array for plot
+        var dataBar = [traceBar];
 
         // Define the plot layout
-        var layout = {
+        var layoutBar = {
             title: "Top 10 OTU results",
             xaxis: { title: "OTU Values" },
             yaxis: { title: "OTU IDs"}
         };
 
         // Plot the chart to a div tag with id "bar"
-        Plotly.newPlot("bar", data, layout);
+        Plotly.newPlot("bar", dataBar, layoutBar);
 
+    //__________________________________________________________________
+        // pull data for bubble chart
+        var traceBubble = {
+            x: filteredId[0].otu_ids,
+            y: filteredId[0].sample_values,
+            mode: "markers",
+            marker: {
+                color: filteredId[0].otu_ids,
+                size: filteredId[0].sample_values
+            },
+            text: filteredId[0].otu_labels
+        };
+
+        var dataBubble = [traceBubble];
+
+        var layoutBubble = {
+            title: "All Sample results for Individual",
+            xaxis: {title: "OTU IDs"}
+        }
+
+        Plotly.newPlot("bubble", dataBubble, layoutBubble);
+    //__________________________________________________________________
         //pull demographic Info by sample
         var metadataData = importedData.metadata;
         var filteredMetadata = metadataData.filter(d => d.id.toString() === dropdownSelection)[0];
         console.log(filteredMetadata);
-        
+            
         //add demographic Info
         var demoInfo = d3.select("#sample-metadata");
-        demoInfo.html = ("");
-        
+        demoInfo.html("");
+        console.log(demoInfo);
+            
         Object.entries(filteredMetadata).forEach((key) => {   
             demoInfo.append("h6").text(key[0] + ": " + key[1] + "\n");
         });
     });
 };
-    
-
-
-
-// d3.json("samples.json").then((importedData)=> {
-//     var namesData = importedData.names;
-//     var samplesData = importedData.samples;
-
-//returns id
-// for (var z = dropdownSelection; z < namesData.length; z++) {
-//     console.log(z);
-//     if(!samplesData.has(z)) {
-//         return z;
-//     }
-// }
-
-
-// var x = [];
-// var y = [];
-
-// for (var z = dropdownSelection; z < namesData.length; z++) {
-//     console.log(z);
-//     if(samplesData.otu_ids === z) {
-//         x = samplesData.sample_values;
-//         y = samplesData.otu_labels;
-//         console.log(x);
-//         console.log(y);
-//     }
-
-
-
-//         // Use filter() to pass the function as its argument
-//         var filteredId = incomingData.filter(filterIndividual);
-
-//   //  Check to make sure your are filtering your movies.
-//   console.log(filteredId);
-
-//   // Use the map method with the arrow function to return all the filtered movie titles.
-//   var otuIds = filteredId.map(id =>  id.samples);
-
-//   // Use the map method with the arrow function to return all the filtered movie metascores.
-//   var sampleValues = filteredId.map(movies => movies.metascore);
-
-//   // Check your filtered metascores.
-//   console.log(ratings);
-
-//   // Create your trace.
-//   var trace = {
-//     x: otuIds,
-//     y: sampleValues,
-//     type: "bar"
-//   };
-
-//   // Create the data array for our plot
-//   var data = [trace];
-
-//   // Define the plot layout
-//   var layout = {
-//     title: "top 10 OTU results",
-//     xaxis: { title: "OTU Values" },
-//     yaxis: { title: "OTU IDs"}
-//   };
-
-//   // Plot the chart to a div tag with id "bar-plot"
-//   Plotly.newPlot("bar-plot", data, layout);
-// });
-
-// };
