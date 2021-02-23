9a7d459ac8e5a3570c1724203322e51d649c8e26
Melanie Nolker <melanie.nolker@kiewit.com>
2021-02-23 14:47:34 -0600

    fixed default page load

--- static/js/app.js
+++ static/js/app.js
@@ -1,39 +1,35 @@
-// Initializes the page with a default plot??
-// function init() {
+// Initializes the page with a default plot
+function init() {
 
-//references JSON data & creates arrays
-d3.json("samples.json").then((importedData)=> {
-    var namesData = importedData.names;
-    var metadataData = importedData.metadata;
-    var samplesData = importedData.samples;
+    //references JSON data & creates arrays
+    d3.json("samples.json").then((importedData)=> {
+        var namesData = importedData.names;
 
-    // console.log(namesData);
-    // console.log(samplesData);
-    // console.log(metadataData);
+        //creates drop down list of subject IDs to select from
+        var select = document.getElementById("selDataset"); 
+        var options = namesData; 
 
+        for(var i = 0; i < options.length; i++) {
+            var opt = options[i];
+            var individual = document.createElement("option");
+            individual.textContent = opt;
+            individual.value = opt;
+            select.appendChild(individual);
+        }
 
-//creates drop down list of subject IDs to select from
-var select = document.getElementById("selDataset"); 
-var options = namesData; 
-
-for(var i = 0; i < options.length; i++) {
-    var opt = options[i];
-    var individual = document.createElement("option");
-    individual.textContent = opt;
-    individual.value = opt;
-    select.appendChild(individual);
-}
-
-});
-// }
-
-// init();
+    
+        d3.select("#selDataset").node().value = options[0];
+        optionChanged();
+    });
+};
 
 function optionChanged() {
     // create variable for drop down selection of ID
-    var dropdownSelection = d3.select("#selDataset").node().value; 
+    var dropdownSel = d3.select("#selDataset").node().value; 
+    if (typeof(dropdownSel) != "undefined") {
+        dropdownSelection = '940'
+    }
 
-    //
     d3.json("samples.json").then((importedData)=> {
         function filterId(individual) {
             return individual.id === dropdownSelection;
@@ -43,7 +39,6 @@ function optionChanged() {
     
         var filteredId = samplesData.filter(filterId);
             console.log(filteredId);
-        
     //__________________________________________________________________
         // bar plot data transformation
             var sampleValuesFiltered = filteredId[0].sample_values.slice(0,10).reverse();
@@ -68,7 +63,7 @@ function optionChanged() {
         // Define the plot layout
         var layoutBar = {
             title: "Top 10 OTU results",
-            xaxis: { title: "OTU Values" },
+            xaxis: { title: "OTU Values"},
             yaxis: { title: "OTU IDs"}
         };
 
@@ -120,3 +115,6 @@ function optionChanged() {
         });
     });
 };
+
+// run to initially load page
+init();
